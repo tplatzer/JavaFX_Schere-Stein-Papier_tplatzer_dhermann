43968eba6f73453ad4dbc405587692314debee79
Tim Platzer <tim.platzer@gmx.at>
2024-11-22 10:58:34 +0100

    added Play Buttons and Progress Indicator

--- src/main/java/htl/steyr/javafx_scheresteinpapier_tplatzer_dhermann/App.java
+++ src/main/java/htl/steyr/javafx_scheresteinpapier_tplatzer_dhermann/App.java
@@ -1,10 +1,18 @@
 package htl.steyr.javafx_scheresteinpapier_tplatzer_dhermann;
 
 import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressIndicator;
 import javafx.stage.Stage;
 
 public class App extends Application
 {
+    public Button rockButton;
+    public Button paperButton;
+    public Button scissorsButton;
+    public Button springButton;
+    public ProgressIndicator kiProgressIndicator;
+
     @Override
     public void start(Stage stage)
     {
