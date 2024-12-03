c0ef74bc36b49701048dcc1fb4d8d76a9f2b4ead
Tim Platzer <tim.platzer@gmx.at>
2024-12-03 08:12:36 +0100

    added funktions for when the player Won, the AI Won and for a Draw renamed some funktions aiTurn -> play generateAiTurn -> aiTurn

--- src/main/java/htl/steyr/javafx_scheresteinpapier_tplatzer_dhermann/Controller.java
+++ src/main/java/htl/steyr/javafx_scheresteinpapier_tplatzer_dhermann/Controller.java
@@ -58,36 +58,50 @@ public class Controller
         showWindow();
     }
 
-    private void play()
+    private void selectWinner()
     {
-        System.out.println();
-        System.out.println("nach UpdateHand");
-
         switch (getWinner())
         {
-
+            case 1 -> playerWon();
+            case 2 -> aiWon();
+            case 0 -> draw();
         }
     }
 
-    private void aiTurn()
+    private void play()
     {
         Thread sleepThread = new Thread(() ->
         {
             try
             {
                 Thread.sleep(3000);
-                generateAiTurn();
+                aiTurn();
                 updateComputerHand();
             } catch (InterruptedException e)
             {
                 e.printStackTrace();
             } finally
             {
-                Platform.runLater(this::play);
+                Platform.runLater(this::selectWinner);
             }
         });
         sleepThread.setDaemon(true);
         sleepThread.start();
+    }
+
+    private void playerWon()
+    {
+
+    }
+
+    private void aiWon()
+    {
+
+    }
+
+    private void draw()
+    {
+
     }
 
     private void showWindow()
@@ -127,7 +141,7 @@ public class Controller
         removeButtonBoxThread.setDaemon(true);
         removeButtonBoxThread.start();
 
-        aiTurn();
+        play();
     }
 
     /**
@@ -162,7 +176,7 @@ public class Controller
                 }
                 case Paper.id, Well.id ->
                 {
-
+                    return 2;
                 }
             }
         }
@@ -289,7 +303,7 @@ public class Controller
         Platform.runLater(() -> getButtonBox().setVisible(false));
     }
 
-    private void generateAiTurn()
+    private void aiTurn()
     {
         Random random = new Random();
         setAiChoice(random.nextInt(4));
@@ -309,127 +323,102 @@ public class Controller
     {
         return maxButtonWidth;
     }
-
     public static int getMaxButtonHeight()
     {
         return maxButtonHeight;
     }
-
     public static int getMaxHboxWidth()
     {
         return maxHboxWidth;
     }
-
     public static int getMaxHboxHeight()
     {
         return maxHboxHeight;
     }
-
     public static int getMaxProgressIndicatorWidth()
     {
         return maxProgressIndicatorWidth;
     }
-
     public static int getMaxProgressIndicatorHeight()
     {
         return maxProgressIndicatorHeight;
     }
-
     public String getUserChoice()
     {
         return userChoice;
     }
-
     public void setUserChoice(String userChoice)
     {
         this.userChoice = userChoice;
     }
-
     public Button getRockButton()
     {
         return rockButton;
     }
-
     public void setRockButton(Button rockButton)
     {
         this.rockButton = rockButton;
     }
-
     public Button getPaperButton()
     {
         return paperButton;
     }
-
     public void setPaperButton(Button paperButton)
     {
         this.paperButton = paperButton;
     }
-
     public Button getScissorsButton()
     {
         return scissorsButton;
     }
-
     public void setScissorsButton(Button scissorsButton)
     {
         this.scissorsButton = scissorsButton;
     }
-
     public Button getWellButton()
     {
         return wellButton;
     }
-
     public void setWellButton(Button wellButton)
     {
         this.wellButton = wellButton;
     }
-
     public ProgressIndicator getEnemieProgressIndicator()
     {
         return enemieProgressIndicator;
     }
-
     public void setEnemieProgressIndicator(ProgressIndicator enemieProgressIndicator)
     {
         this.enemieProgressIndicator = enemieProgressIndicator;
     }
-
     public VBox getRoot()
     {
         return this.root;
     }
-
     public void setRoot(VBox root)
     {
         this.root = root;
     }
-
     public Stage getStage()
     {
         return this.stage;
     }
-
     public void setStage(Stage stage)
     {
         this.stage = stage;
     }
-
     public Controller getController()
     {
         return controller;
     }
-
     public void setController(Controller controller)
     {
         this.controller = controller;
     }
-
     public String getAiChoice()
     {
         return aiChoice;
     }
-
     public void setAiChoice(int aiChoice)
     {
         switch (aiChoice)
@@ -441,62 +430,50 @@ public class Controller
         }
 
     }
-
     public HashMap<Integer, String> getPlayStonesIDs()
     {
         return playStonesIDs;
     }
-
     public void setPlayStonesIDs(HashMap<Integer, String> playStonesIDs)
     {
         this.playStonesIDs = playStonesIDs;
     }
-
     public HBox getProgressBox()
     {
         return progressBox;
     }
-
     public void setProgressBox(HBox progressBox)
     {
         this.progressBox = progressBox;
     }
-
     public HBox getEnemyBox()
     {
         return enemyBox;
     }
-
     public void setEnemyBox(HBox enemyBox)
     {
         this.enemyBox = enemyBox;
     }
-
     public HBox getTableBox()
     {
         return tableBox;
     }
-
     public void setTableBox(HBox tableBox)
     {
         this.tableBox = tableBox;
     }
-
     public HBox getButtonBox()
     {
         return buttonBox;
     }
-
     public void setButtonBox(HBox buttonBox)
     {
         this.buttonBox = buttonBox;
     }
-
     public ImageView getComputerHand()
     {
         return computerHand;
     }
-
     public void setComputerHand(ImageView computerHand)
     {
         this.computerHand = computerHand;
