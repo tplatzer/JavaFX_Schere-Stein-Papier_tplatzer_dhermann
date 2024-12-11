a81b1f3d5a5c23264d4b2733bc73338788b73558
Sqidoodle <neinv12@gmail.com>
2024-12-11 16:39:32 +0100

    the setStyle() methods have been moved to a .css file and the gameEnd field has been refined

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,11 @@
 module htl.steyr.javafx_scheresteinpapier_tplatzer_dhermann {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     requires org.controlsfx.controls;
     requires java.desktop;
 
-    opens htl.steyr.javafx_scheresteinpapier_tplatzer_dhermann to javafx.fxml;
+    opens htl.steyr.javafx_scheresteinpapier_tplatzer_dhermann to javafx.fxml, javafx.graphics;
     exports htl.steyr.javafx_scheresteinpapier_tplatzer_dhermann;
 }
